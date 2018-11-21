8ba3bcacea1f0880655b833cd8fda15e5f0a70ae
william <willshinji@gmail.com>
2018-11-21 00:17:40 -0200

    react state

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import api from '../../services/api'
 
 export default class Main extends Component {
+  state = {
+    products: []
+  }
   componentDidMount(){
     this.loadProducts();
   }
 
   loadProducts  = async () => {
     const response = await api.get('/products');
-    console.log(response.data.docs)
+    this.setState({products: response.data.docs})
   }
   render(){
-    return (<h1>Hello Rocketseat</h1>)
+    return (<h1>Contagem de Produtos {this.state.products.length}</h1>)
   }
 }
