f342add5db93fa272fc856b97b6ebdf1ed748f27
william <willshinji@gmail.com>
2018-12-03 13:32:00 -0200

    add route /

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css'
 import Header from './components/Header'
-import Main from './pages/main'
+import Routes from './routes'
 
 class App extends Component {
   render() {
     return (     
       <div className="App">   
         <Header />  
-        <Main />
+        <Routes />
       </div>
     );
   }
